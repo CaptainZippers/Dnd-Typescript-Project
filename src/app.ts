7799bc9e7a0dce4262e5fb706cee2caa2828fbf9
Nigel Krajcer <nphilmont2010@gmail.com>
2021-06-06 14:12:49 -0400

    Render basic form using OOP styled code and add in favicon

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-// Code goes here!
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
+        this.hostElement = <HTMLDivElement>document.getElementById('app')!;
+        //import FormElement and render Form
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = <HTMLFormElement>importedNode.firstElementChild
+        this.attach();
+
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const prjInput = new ProjectInput()
